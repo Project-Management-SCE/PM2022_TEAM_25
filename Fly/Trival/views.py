4f40094dc7f32b35f83531e29992da8b7e2353b9
yehia1997 <yehians@ac.sce.ac.il>
2022-04-14 13:19:18 +0300

    BSPM2022T25-58 log in manger backend

--- Fly/Trival/views.py
+++ Fly/Trival/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth import logout
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.shortcuts import render, redirect
 
+from django.http import HttpResponseRedirect
 # Create your views here.
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('login')
+    return render(request,'homePage.html')
+
+def afterlogin(request):
+    if request.user.is_authenticated == False:
+        print("asdasdsadasdas")
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST.get('password1')
+            user = auth.authenticate(username=username, password=password)
+            if user.is_staff:
+                auth.login(request, user)
+                return redirect('HomePageadmin')
+
+
+
+
+
+    else:
+        if  request.user.is_staff:
+            return redirect('HomePageadmin')
